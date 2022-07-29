c29aa1260473de8655d50df49b2cd4c250422e63
Riccardo Attilio Galli <riccardo@sideralis.org>
2022-07-28 21:31:25 -0700

    Rewrite build_path_to_dependency to be more complete

--- src/main.rs
+++ src/main.rs
@@ -50,34 +50,64 @@ struct Opt {
 
 type Pkg<'a> = (&'a str, &'a str);
 
-#[allow(clippy::ptr_arg)]
-fn build_path_to_dependency<'a>(
+fn get_parents<'a>(
+    pkg: &'a Pkg<'a>,
+    pkg2parents: &'a HashMap<&'a Pkg<'a>, ParentsNode>,
+) -> Cow<'a, Vec<&'a Pkg<'a>>> {
+    if let Some(parents_node) = pkg2parents.get(pkg) {
+        Cow::Borrowed(&parents_node.desc)
+    } else {
+        Cow::Owned(Vec::new())
+    }
+}
+
+fn _build_path_to_dependency<'a>(
     pkg: &'a Pkg<'a>,
     pkg2parents: &'a HashMap<&'a Pkg<'a>, ParentsNode>,
     curr_path: &mut Vec<&'a Pkg<'a>>,
     paths: &mut Vec<Vec<&'a Pkg<'a>>>,
+    visited: &mut HashMap<&'a Pkg<'a>, usize>,
 ) {
+    *visited.entry(pkg).or_insert(0) += 1;
     curr_path.push(pkg);
+    let parents = get_parents(pkg, pkg2parents);
 
-    let parents_node = pkg2parents.get(pkg);
-    if parents_node.is_some() && !parents_node.unwrap().desc.is_empty() {
-        // color the parent while traversing. If we already covered it in a
-        // previous path, drop it. This is actually necessary not only to
-        // reduce the overall output, but to solve dependency loops.
-        if *pkg2parents[pkg].traversed.borrow() {
-            curr_path.pop();
-        } else {
-            *pkg2parents[pkg].traversed.borrow_mut() = true;
-            for p in pkg2parents[pkg].desc.iter() {
-                build_path_to_dependency(p, pkg2parents, curr_path, paths);
+    if parents.is_empty() {
+        // root dependency (no parents, it must have been defined in package.json)
+        let mut complete_path = curr_path.clone();
+        complete_path.reverse();
+        complete_path.truncate(
+            complete_path
+                .iter()
+                .position(|n| n == complete_path.last().unwrap())
+                .unwrap()
+                + 1,
+        );
+        paths.push(complete_path);
+    } else {
+        for p in parents.iter() {
+            if *visited.get(p).unwrap_or(&0_usize) < 20 {
+                _build_path_to_dependency(p, pkg2parents, curr_path, paths, visited);
             }
         }
-    } else {
-        let mut res = curr_path.clone();
-        res.reverse();
-        paths.push(res);
-        curr_path.pop();
     }
+
+    curr_path.pop();
+}
+
+fn build_path_to_dependency<'a>(
+    pkg: &'a Pkg<'a>,
+    pkg2parents: &'a HashMap<&'a Pkg<'a>, ParentsNode>,
+    paths: &mut Vec<Vec<&'a Pkg<'a>>>,
+) {
+    if !pkg2parents.contains_key(pkg) {
+        return;
+    }
+
+    let mut visited: HashMap<&'a Pkg<'a>, usize> = HashMap::new();
+
+    let mut path = Vec::new();
+    _build_path_to_dependency(pkg, pkg2parents, &mut path, paths, &mut visited);
 }
 
 fn why<'a>(
@@ -91,8 +121,9 @@ fn why<'a>(
 
     let mut paths: Vec<Vec<&Pkg>> = Vec::new();
     for q in queries.iter() {
-        let mut curr_path: Vec<&Pkg> = Vec::new();
-        build_path_to_dependency(q, pkg2parents, &mut curr_path, &mut paths);
+        if pkg2parents.get(q).is_some() {
+            build_path_to_dependency(q, pkg2parents, &mut paths);
+        }
     }
 
     if paths.len() == 1 && paths.get(0).unwrap().len() == 1 {
@@ -118,7 +149,6 @@ fn why<'a>(
 
 #[derive(Debug)]
 struct ParentsNode<'a> {
-    traversed: RefCell<bool>,
     desc: Vec<&'a (&'a str, &'a str)>,
 }
 
@@ -220,10 +250,7 @@ fn main() -> Result<()> {
     for e in entries.iter() {
         for dep in e.dependencies.iter() {
             if !pkg2parents.contains_key(dep) {
-                let parents_node = ParentsNode {
-                    traversed: RefCell::new(false),
-                    desc: Vec::new(),
-                };
+                let parents_node = ParentsNode { desc: Vec::new() };
                 pkg2parents.insert(dep, parents_node);
             }
 
@@ -407,4 +434,123 @@ fn convert_paths_to_tree<'a>(paths: &'a [Vec<&Pkg<'a>>]) -> Vec<Rc<RefCell<Node<
         }
     }
     output
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const PKG_A: Pkg = ("a", "v1");
+    const PKG_B: Pkg = ("b", "v1");
+    const PKG_C: Pkg = ("c", "v1");
+    const PKG_D: Pkg = ("d", "v1");
+
+    fn mock_pkg2parents_empty<'a>() -> HashMap<&'a Pkg<'a>, ParentsNode<'a>> {
+        let pkg2parents: HashMap<&Pkg, ParentsNode> = HashMap::new();
+        pkg2parents
+    }
+
+    fn mock_pkg2parents_one_element<'a>() -> HashMap<&'a Pkg<'a>, ParentsNode<'a>> {
+        let mut pkg2parents: HashMap<&Pkg, ParentsNode> = HashMap::new();
+        let p = ParentsNode { desc: Vec::new() };
+        pkg2parents.insert(&PKG_A, p);
+        pkg2parents
+    }
+
+    fn mock_pkg2parents_ab<'a>() -> HashMap<&'a Pkg<'a>, ParentsNode<'a>> {
+        let mut pkg2parents: HashMap<&Pkg, ParentsNode> = HashMap::new();
+        let p = ParentsNode { desc: vec![&PKG_B] };
+        pkg2parents.insert(&PKG_A, p);
+        pkg2parents
+    }
+
+    fn mock_pkg2parents_ab_ac<'a>() -> HashMap<&'a Pkg<'a>, ParentsNode<'a>> {
+        let mut pkg2parents: HashMap<&Pkg, ParentsNode> = HashMap::new();
+        let p = ParentsNode {
+            desc: vec![&PKG_B, &PKG_C],
+        };
+        pkg2parents.insert(&PKG_A, p);
+        pkg2parents
+    }
+
+    fn mock_pkg2parents_abc_abdc<'a>() -> HashMap<&'a Pkg<'a>, ParentsNode<'a>> {
+        let mut pkg2parents: HashMap<&Pkg, ParentsNode> = HashMap::new();
+        let c_parents = ParentsNode {
+            desc: vec![&PKG_D, &PKG_B],
+        };
+        let d_parents = ParentsNode { desc: vec![&PKG_B] };
+        let b_parents = ParentsNode { desc: vec![&PKG_A] };
+        pkg2parents.insert(&PKG_C, c_parents);
+        pkg2parents.insert(&PKG_D, d_parents);
+        pkg2parents.insert(&PKG_B, b_parents);
+        pkg2parents
+    }
+
+    #[test]
+    fn pkg_not_found() {
+        let query: Pkg = ("foo", "1");
+        let pkg2parents = mock_pkg2parents_empty();
+        let mut paths: Vec<Vec<&Pkg>> = Vec::new();
+
+        build_path_to_dependency(&query, &pkg2parents, &mut paths);
+
+        let output: Vec<Vec<&Pkg>> = Vec::new();
+
+        assert_eq!(paths, output);
+    }
+
+    #[test]
+    fn pkg_no_parents() {
+        let query = &PKG_A;
+        let pkg2parents = mock_pkg2parents_one_element();
+        let mut paths: Vec<Vec<&Pkg>> = Vec::new();
+
+        build_path_to_dependency(query, &pkg2parents, &mut paths);
+
+        let output = vec![vec![&PKG_A]];
+
+        assert_eq!(paths, output);
+    }
+
+    #[test]
+    fn pkg_one_parent() {
+        let query: &Pkg = &PKG_A;
+        let pkg2parents = mock_pkg2parents_ab();
+        let mut paths: Vec<Vec<&Pkg>> = Vec::new();
+
+        build_path_to_dependency(query, &pkg2parents, &mut paths);
+
+        let output: Vec<Vec<&Pkg>> = vec![vec![&PKG_B, &PKG_A]];
+
+        assert_eq!(paths, output);
+    }
+
+    #[test]
+    fn pkg_two_parents() {
+        let query: &Pkg = &PKG_A;
+        let pkg2parents = mock_pkg2parents_ab_ac();
+        let mut paths: Vec<Vec<&Pkg>> = Vec::new();
+
+        build_path_to_dependency(query, &pkg2parents, &mut paths);
+
+        let output: Vec<Vec<&Pkg>> = vec![vec![&PKG_B, &PKG_A], vec![&PKG_C, &PKG_A]];
+
+        assert_eq!(paths, output);
+    }
+
+    #[test]
+    fn pkg_two_branches() {
+        let query: &Pkg = &PKG_C;
+        let pkg2parents = mock_pkg2parents_abc_abdc();
+        let mut paths: Vec<Vec<&Pkg>> = Vec::new();
+
+        build_path_to_dependency(query, &pkg2parents, &mut paths);
+
+        let output: Vec<Vec<&Pkg>> = vec![
+            vec![&PKG_A, &PKG_B, &PKG_D, &PKG_C],
+            vec![&PKG_A, &PKG_B, &PKG_C],
+        ];
+
+        assert_eq!(paths, output);
+    }
 }
